80ed84c733cee52cba9f398c8939151a8a84fc76
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-12-01 07:42:14 -0800

    Task 6 - program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import sys
 
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
+    argsc = len(sys.argv) - 1
+    if argsc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
