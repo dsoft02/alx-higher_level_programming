6f0b8ff3f619bc8f731544312ed9d7303d370e7b
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-03-14 10:37:53 -0700

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length === 5) {
+  fs.writeFile(process.argv[4], '', { flag: 'wx' }, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      fs.appendFile(process.argv[4], data, (err) => {
+        if (err) {
+          throw (err);
+        }
+      });
+    }
+  });
+  fs.readFile(process.argv[3], (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      fs.appendFile(process.argv[4], data, (err) => {
+        if (err) {
+          throw (err);
+        }
+      });
+    }
+  });
+}
